dd404bfddac08563e81193bfd670ce9a90a7eea3
rishondabreo <richardmd73@student.sfit.ac.in>
2025-07-01 18:54:33 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Rishon Dabreo',
+    author_email='rishon.deabreo@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
